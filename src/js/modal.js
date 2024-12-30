722d6df058c00136fc8cd398a9b188a478759ae2
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-12-30 17:56:22 +0200

    update

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,11 @@ const openButtons = document.querySelectorAll('[data-modal]');
 const closeButtons = document.querySelectorAll('.close-button');
 
 function toggleModal(e) {
-  const modalName = e.target.dataset.modal;
+  const button =
+    e.target.closest('[data-modal]') || e.target.closest('.close-button');
+  if (!button) return;
+
+  const modalName = button.dataset.modal || button.closest('.backdrop').id;
   const modal = document.getElementById(modalName);
 
   if (modal) {
